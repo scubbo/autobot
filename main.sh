1b971da387f9ee01c61e59c46f1fca07ca38b354
Jack Jackson <jackjack@amazon.com>
2017-09-20 00:05:07 -0700

    Lambda creation

--- main.sh
+++ main.sh
@@ -30,12 +30,26 @@ if [[ -z $PROJECT_NAME ]]; then
 fi
 
 # Create Lambda Role
-EXISTING_LAMBDA_ROLE_ARN=$(aws iam list-roles | jq -r --arg ROLE_NAME $PROJECT_NAME"-lambda_role" '.Roles[] | select(.RoleName==$ROLE_NAME) | .Arn')
-if [[ -z $EXISTING_LAMBDA_ROLE_ARN ]]; then
-  EXISTING_LAMBDA_ROLE_ARN=$(aws iam create-role --role-name $PROJECT_NAME"-lambda_role" --assume-role-policy-document '{"Version":"2012-10-17","Statement":[{"Action":"sts:AssumeRole","Effect":"Allow","Principal":{"Service":"lambda.amazonaws.com"}}]}' | jq -r '.Role .Arn');
+LAMBDA_ROLE_NAME=$PROJECT_NAME"_lambda-role";
+LAMBDA_ROLE_ARN=$(aws iam list-roles | jq -r --arg ROLE_NAME $LAMBDA_ROLE_NAME '.Roles[] | select(.RoleName==$ROLE_NAME) | .Arn');
+if [[ -z $LAMBDA_ROLE_ARN ]]; then
+  LAMBDA_ROLE_ARN=$(aws iam create-role --role-name $LAMBDA_ROLE_NAME --assume-role-policy-document '{"Version":"2012-10-17","Statement":[{"Action":"sts:AssumeRole","Effect":"Allow","Principal":{"Service":"lambda.amazonaws.com"}}]}' --description "Created by $SCRIPT_NAME" | jq -r '.Role .Arn');
   sleep 2; # https://stackoverflow.com/a/37438525/1040915 :(
 fi
 
 # Create function
+LAMBDA_FUNCTION_NAME=$PROJECT_NAME"_lambda-function";
+LAMBDA_FUNCTION_ARN=$(aws lambda list-functions | jq -r --arg FUNCTION_NAME $LAMBDA_FUNCTION_NAME '.Functions[] | select(.FunctionName==$FUNCTION_NAME) | .FunctionArn');
+if [[ -z $LAMBDA_FUNCTION_ARN ]]; then
+  # Lambda creation requires a non-empty zip file
+  # TODO we can probably get rid of this altogether if we've already created a repo by this point
+  mkdir /tmp/"$PROJECT_NAME""_temp";
+  cd /tmp/"$PROJECT_NAME""_temp";
+  echo 'hello' >> foo
+  zip lambda.zip foo
+  LAMBDA_FUNCTION_ARN=$(aws lambda create-function --function-name $LAMBDA_FUNCTION_NAME --runtime python2.7 --role $LAMBDA_ROLE_ARN --zip-file fileb://lambda.zip --handler main.lambda_handler --description "Created by $SCRIPT_NAME" | jq -r '.FunctionArn');
+  cd -
+  rm -rf /tmp/"$PROJECT_NAME""_temp";
+fi
 
 # Create API
