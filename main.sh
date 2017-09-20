19f7b929fecd10379850a975f8f2345e764134b3
Jack Jackson <jackjack@amazon.com>
2017-09-19 23:22:48 -0700

    First commit

---
+++ main.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPT_NAME="AutoBot"
+
+# https://stackoverflow.com/a/677212/1040915
+hash jq 2>/dev/null || { echo >&2 "This script requires jq, but it's not installed.  Try getting it from https://github.com/stedolan/jq/wiki/Installation. Aborting."; exit 1; }
+hash aws 2>/dev/null || { echo >&2 "This script requires aws, but it's not installed.  Aborting."; exit 1; }
+
+set -e
+
+# Create role
+
+# Create function
+
+# Create API
